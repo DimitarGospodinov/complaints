514e1f5d49e85d78935672fe8185c54820b5f1a0
Dimi <melloxyz@gmail.com>
2022-11-22 16:18:46 +0200

    added db table for transactions

---
+++ models/transactions.py
@@ -0,0 +1,13 @@
+import sqlalchemy
+from db import metadata
+
+transaction = sqlalchemy.Table(
+    "transactions",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("quote_id", sqlalchemy.String(120), nullable=False),
+    sqlalchemy.Column("transfer_id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("target_account_id", sqlalchemy.String(100), primary_key=True),
+    sqlalchemy.Column("amount", sqlalchemy.Float, primary_key=True),
+    sqlalchemy.Column("complaint_id", sqlalchemy.ForeignKey("complaints.id"))
+)
